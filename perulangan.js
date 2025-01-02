2acbfb7f6b41509e3ad405a523bb8b0f6a0c6b34
syifaa019 <144194434+syifaa019@users.noreply.github.com>
2025-01-02 17:16:45 +0700

    Add files via upload

---
+++ perulangan.js
@@ -0,0 +1,3 @@
+for (let i = 15; i >= 1; i--) {
+    console.log(`Angka: ${i}`);
+}
---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $("#autoWidth").lightSlider({
+    adaptiveHeight: true,
+    auto: true,
+    item: 1,
+    slideMargin: 0,
+    loop: true,
+  });
+});
+
+const navbarHome = document.querySelector(".navbarHome");
+
+window.addEventListener("scroll", () => {
+  const post = window.scrollY > 100;
+});
+
+navbarHome.classList.toggle("scroll", post);
+// Tampilkan alert dengan pesan "Sudah Terkirim"
+alert('Pesan Anda sudah terkirim!');
